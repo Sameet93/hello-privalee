0039af2447b815b6a43009d1c726c032ee69dd47
Sameet Naik <sameet@trepidustech.com>
2024-04-05 13:15:01 +0400

    health check and improvements

--- main.go
+++ main.go
@@ -11,5 +11,11 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Privilee!")
 	})
+	e.GET("/health", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Healthy")
+	})
+	e.GET("/ready", func(c echo.Context) error {
+		return c.String(http.StatusOK, "ready")
+	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
